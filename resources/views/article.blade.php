687543f6b4c8b852052271ae086f089da978eaef
Yann-ship-it <yy.iris85@gmail.com>
2023-05-15 15:10:25 +0200

    Utilisation de vite pour le CSS et update commentaire

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
     <h1>{{ $post->content }}</h1>
-
+    <span>{{ $post->image ? $post->image->path : "Aucune image" }}</span>
     @forelse ($post->comments as $comment)
-        <h2 style="font-style: italic">{{  $comment->content }} envoyé le {{ $comment->created_at->format('d/m/Y') }}</h2>
+
+        <div>
+            <h2 style="font-style: italic">{{  $comment->content }} envoyé le {{ $comment->created_at->format('d/m/Y') }}</h2>
+        </div>
     @empty
         <span>Aucun commentaire pour ce post</span>
     @endforelse
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @include('partials.navbar')
     @yield('content')
-    <script src="{{ asset('js.app.js') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </body>
 </html>
